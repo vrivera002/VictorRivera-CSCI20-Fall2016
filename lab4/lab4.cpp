2067a6d62b1b79dbf83fa35f1a39665e028e43be
Victor Rivera <vrivera002@student.butte.edu>
2016-08-30 16:36:24 +0000

    Lab4 submission

---
+++ lab4/lab4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Johhny the (insert integer)th" << endl;
+    cout << "child, really hated the number (insert floating point)!" << endl;
+    cout << "He would prefer it be called (insert character here)" << endl;
+    cout << "For forever and eternity. Till the cookie crumbles." << endl;
+    cout << "(insert string here)." << endl;
+    
+}

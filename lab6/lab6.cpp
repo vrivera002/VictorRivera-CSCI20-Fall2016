8a2639991f8e12ea85b773d2391efbda68483c04
Victor Rivera <vrivera002@student.butte.edu>
2016-09-19 17:12:27 +0000

    Lab6 submission

---
+++ lab6/lab6.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+void asciiArt ()
+{
+     cout <<"      ###" << endl;
+   cout <<"     # __ #" << endl;
+   cout <<"     # .. #" << endl;
+   cout <<"     # __ #" << endl;
+   cout <<"     #    #" << endl;
+cout <<"  ##   ##   "<< endl;
+  cout <<"     $$$##$$$  copyright Victor Rivera 2016"<< endl;
+   cout <<"       ##   ##"<< endl;
+   cout <<"       ##   "<< endl;
+   cout <<"       ##   "<< endl;
+   cout <<"      #  #  "<< endl;
+   cout <<"      #  #  "<< endl;
+   cout <<"      #  #  "<< endl;
+};
+int main()
+{
+  asciiArt();
+}  
